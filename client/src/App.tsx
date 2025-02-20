3b10eaac54d6392e88763ebe7526bb53c2842e81
joecapels <38777446-joecapels@users.noreply.replit.com>
2025-02-20 14:45:08 +0000

    Restored to '571362dc2a5d3498a95e3ccc00c7c295d4b63d96'
    Replit-Restored-To:571362dc2a5d3498a95e3ccc00c7c295d4b63d96

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,6 @@ import BadgesPage from "@/pages/badges-page";
 import AdminPage from "@/pages/admin-page";
 import NotificationsPage from "@/pages/notifications-page";
 import UpgradesPage from "@/pages/upgrades-page";
-import SkillTreePage from "@/pages/skill-tree-page";
 
 function Router() {
   return (
@@ -35,7 +34,6 @@ function Router() {
       <ProtectedRoute path="/notifications" component={NotificationsPage} />
       <ProtectedRoute path="/admin" component={AdminPage} />
       <ProtectedRoute path="/upgrades" component={UpgradesPage} />
-      <ProtectedRoute path="/skill-tree" component={SkillTreePage} />
       <Route path="/auth" component={AuthPage} />
       <Route component={NotFound} />
     </Switch>
